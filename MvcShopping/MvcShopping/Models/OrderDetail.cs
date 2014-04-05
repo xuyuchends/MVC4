37796a0dafb933760715cda31af07a5a080c7a72
xuyuchen <xuyuchends@gmail.com>
2014-04-05 10:43:51 +0800

    dd

---
+++ MvcShopping/MvcShopping/Models/OrderDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace MvcShopping.Models
+{
+    [DisplayName("訂單明細")]
+    [DisplayColumn("Name")]
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("訂單主檔")]
+        [Required]
+        public OrderHeader OrderHeader { get; set; }
+
+        [DisplayName("訂購商品")]
+        [Required]
+        public Product Product { get; set; }
+
+        [DisplayName("商品售價")]
+        [Required(ErrorMessage = "請輸入商品售價")]
+        [Range(99, 10000, ErrorMessage = "商品售價必須介於 99 ~ 10,000 之間")]
+        [Description("由於商品售價可能會經常異動，因此必須保留購買當下的商品售價")]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+    }
+}
---
+++ MvcShopping/MvcShopping/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcShopping.Models
+{
+    [DisplayName("商品類別")]
+    [DisplayColumn("Name")]
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("商品類別名稱")]
+        [Required(ErrorMessage = "請輸入商品類別名稱")]
+        [MaxLength(20, ErrorMessage="類別名稱不可超過20個字")]
+        public string Name { get; set; }
+    }
+}
