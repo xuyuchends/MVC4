33424596bda5fb64e3753c4a40f06ade433493f2
xuyuchen <xuyuchends@gmail.com>
2014-04-07 23:55:19 +0800

    ggg

---
+++ MvcShopping/MvcShopping/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcShopping.Controllers
+{
+    public class CartController : Controller
+    {
+        // 加入產品項目到購物車，如果沒有傳入 Amount 參數則預設購買數量為 1
+        [HttpPost] // 因為知道要透過 AJAX 呼叫這個 Action，所以可以先標示 [HttpPost] 屬性
+        public ActionResult AddToCart(int ProductId, int Amount = 1)
+        {
+            return View();
+        }
+
+        // 顯示目前的購物車項目
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 移除購物車項目
+        [HttpPost] // 因為知道要透過 AJAX 呼叫這個 Action，所以可以先標示 [HttpPost] 屬性
+        public ActionResult Remove(int ProductId)
+        {
+            return View();
+        }
+
+        // 更新購物車中特定項目的購買數量
+        [HttpPost] // 因為知道要透過 AJAX 呼叫這個 Action，所以可以先標示 [HttpPost] 屬性
+        public ActionResult UpdateAmount(int ProductId, int NewAmount)
+        {
+            return View();
+        }
+    }
+}
--- MvcShopping/MvcShopping/Controllers/HomeController.cs
+++ MvcShopping/MvcShopping/Controllers/HomeController.cs
@@ -13,19 +13,61 @@ namespace MvcShopping.Controllers
         // 首頁
         public ActionResult Index()
         {
-            return View();
+            var data = new List<ProductCategory>()
+            {
+                new ProductCategory(){Id=1,Name="文具"},
+                 new ProductCategory(){Id=2,Name="礼品"},
+                  new ProductCategory(){Id=3,Name="数据"},
+                   new ProductCategory(){Id=4,Name="书籍"}
+            };
+            return View(data );
         }
 
         // 商品列表
         public ActionResult ProductList(int id)
         {
-            return View();
+            var productCategory = new ProductCategory() { Id = id, Name = "类别 " + id };
+            var data = new List<Product>()
+            {
+                new Product()
+                {
+                    Id=1,
+                    ProductCategory=productCategory,
+                    Name="原子笔",
+                    Description="N/A",
+                    Price=30,
+                    PublishOn=DateTime.Now,
+                    Color=Color.Black
+                },
+                                new Product()
+                {
+                    Id=2,
+                    ProductCategory=productCategory,
+                    Name="铅笔",
+                    Description="N/A",
+                    Price=5,
+                    PublishOn=DateTime.Now,
+                    Color=Color.Black
+                }
+            };
+            return View(data);
         }
 
         // 商品明細
         public ActionResult ProductDetail(int id)
         {
-            return View();
+            var productCategory = new ProductCategory() { Id = id, Name = "文具" };
+            var data =new Product()
+            {
+                    Id=id,
+                    ProductCategory=productCategory,
+                    Name="商品"+id,
+                    Description="N/A",
+                    Price=30,
+                    PublishOn=DateTime.Now,
+                    Color=Color.Black
+                };
+            return View(data);
         }
     }
 }
